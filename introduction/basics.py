36d4d0d4cef9bd7d4754556343546ab339ac93da
Daniel <3138740851@qq.com>
2020-12-29 12:05:16 -0500

    work on basics

---
+++ introduction/basics.py
@@ -0,0 +1,11 @@
+print("hello world") # simple console log
+
+print (5 * 7)
+
+print("hello" + " world") # can concatante strings just like in javascript
+
+# greetings = "Hello"
+# name = input("please enter your name?")
+# print(greetings + " " + name)
+
+print("1\n2\n3\n4\n5") # escaping
