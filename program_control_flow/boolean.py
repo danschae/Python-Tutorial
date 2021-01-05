c3edd42deb230a88276f032ff33aa715254d3d20
Daniel <3138740851@qq.com>
2021-01-05 13:18:25 -0500

    add python boolean

---
+++ program_control_flow/boolean.py
@@ -0,0 +1,8 @@
+day = "Saturday"
+temp = 25
+raining = False
+
+if day == "Saturday" and temp > 27 or not raining: # not is falsy(duh, but no need to write !)
+  print("go swimming")
+else:
+  print("Don't go swimming")
